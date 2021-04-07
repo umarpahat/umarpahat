022cfb94392c38166330e5a6a69d361200d96d7d
prateekeshwar <prateekeshwar@paymeindia.in>
2021-04-07 19:00:39 +0530

    ref

--- build-utils/webpack.common.js
+++ build-utils/webpack.common.js
@@ -30,7 +30,7 @@ module.exports = {
   },
   plugins: [new webpack.HotModuleReplacementPlugin(), new HtmlWebpackPlugin({
     template: path.resolve(__dirname, '..', './src/index.html'),
-    favicon: path.resolve(__dirname, '..', './src/images/favicon.ico')
+    favicon: path.resolve(__dirname, '..', './src/image/favicon.ico')
   }), new CleanWebpackPlugin()],
   output: {
     path: path.resolve(__dirname, '..', './dist'),
