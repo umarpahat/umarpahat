5d270a6d7327daa286180abccffbe61dec22ddbd
pramodsingh11 <you@example.com>
2021-07-08 16:42:17 +0530

    endpoint issue resolved

--- src/constant.js
+++ src/constant.js
@@ -1,5 +1,5 @@
 // export const API_ENDPOINT = 'http://localhost:8000';  
-export const API_ENDPOINT = 'https://api.testing.paymeindia.in'
+export const API_ENDPOINT = 'https://api.paymeindia.in'
 // export const API_ENDPOINT = 'https:///apiv2.paymeindia.in';
 // export const API_ENDPOINT = 'https://apit.paymeindia.in';
 //export const API_ENDPOINT = 'http://testing.paymeindia.in';
