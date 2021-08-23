d7e3a0a099d4aacd84207c7c950b1168189ac7b3
Umar <umar.pahat@paymeindia.in>
2021-08-23 15:54:29 +0530

    Added 404 page.

--- src/pages/404.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-function Four04() {
-  return <h1>OOps ! 404 Page not found</h1>;
-}
-export default Four04;
---
+++ src/pages/NotFoundPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+function NotFoundPage() {
+  return <>
+    <h3>404 page not found</h3>
+    <p>We are sorry but the page you are looking for does not exist.</p>
+  </>;
+}
+export default NotFoundPage;
