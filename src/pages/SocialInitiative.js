eaec77df8c884878a16c512579270aa08eacf411
Umar <umar.pahat@paymeindia.in>
2021-08-25 11:56:29 +0530

    validation and error messge show.

--- src/pages/SocialInitiative.js
+++ src/pages/SocialInitiative.js
@@ -11,7 +11,16 @@ import saarthi from "../images/sarthi-logo.png";
 import MetaTags from "react-meta-tags";
 import {API_ENDPOINT} from "../constant";
 import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
+toast.configure();
+const options = {
+    position: toast.POSITION.TOP_CENTER,
+    autoClose: 6000,
+    limit: 1,
+    closeButton: false,
+};
 const SocialInitiative = (props) => {
     let [loader, setloader] = useState(false);
     const [toggle, setToggle] = useState("");
@@ -22,10 +31,36 @@ const SocialInitiative = (props) => {
     let url = '';
 
     const postVolunteer = () => {
-        if(toggle==='true'){
-             url  = `http://3.6.55.160:3000/api/register-volunteer`;
+        if (name.length === 0) {
+            toast.error("Name can't be empty");
+            return false
+        }
+        if (email.length < 5) {
+             toast.error("Email should be at least 5 charcters long");
+            return false
+        }
+        if (email.split("").filter(x => x === "@").length !== 1) {
+             toast.error("Email should contain a @");
+            return false
+        }
+        if (email.indexOf(".") === -1) {
+             toast.error("Email should contain at least one dot");
+            return false
+        }
+        if (phone.length === 0) {
+             toast.error("Phone can't be empty");
+            return false
+        }
+        if (topic.length === 0) {
+             toast.error("Topic can't be empty");
+            return false
+        }
+
+
+        if (toggle === 'true') {
+            url = `http://3.6.55.160:3000/api/register-volunteer`;
         } else {
-             url  = `http://3.6.55.160:3000/api/register-trainee`;
+            url = `http://3.6.55.160:3000/api/register-trainee`;
         }
         let data = {
             name: name,
@@ -42,12 +77,12 @@ const SocialInitiative = (props) => {
 
         axios
             .post(url, data, config)
-            .then((res) => {
-                console.log(res.data.data);
+            .then(function (response) {
+                toast.success('Success Submitted');
+                document.getElementById("form").reset();
             })
-
-            .catch((err) => {
-                console.log(err);
+            .catch(function (error) {
+                toast.error(error.response.data.message);
             });
     };
 
@@ -56,9 +91,11 @@ const SocialInitiative = (props) => {
             <Header {...props} />
             <MetaTags>
                 <title>Social Initiative</title>
-                <meta name="description" content="PayMe India launches project ‘Saarthi’ to support families impacted by Coivd-19, through skill building and creation of employment opportunities." />
-                <meta name="keyword" content="personal loans online, quick personal loans, instant personal loan, small personal loans, instant personal loan online, instant loan online" />
-                <meta property="og:title" content="Social Initiative" />
+                <meta name="description"
+                      content="PayMe India launches project ‘Saarthi’ to support families impacted by Coivd-19, through skill building and creation of employment opportunities."/>
+                <meta name="keyword"
+                      content="personal loans online, quick personal loans, instant personal loan, small personal loans, instant personal loan online, instant loan online"/>
+                <meta property="og:title" content="Social Initiative"/>
             </MetaTags>
             <div className='content'>
                 <div className="services">
@@ -74,18 +111,41 @@ const SocialInitiative = (props) => {
                                     src={saarthi}
                                 />
                             </div>
-                            <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12  initiative" style={{textAlign:"justify"}}>
-                                <h4>PayMe India launches project ‘Saarthi’ to support families impacted by Coivd-19, through skill building and creation of employment opportunities.</h4>
-                                <p className='p-t-15'>We are facing a global health crisis unlike any we have seen in the last many decades. One that is killing people, spreading human suffering, and upending people’s lives. But much more than a health crisis, it is a human, economic and social crisis which is attacking societies at their core.</p>
-                                <p className='p-t-15'>With casualties in India touching as high as 4000 per day, numerous families have been impacted severely and more so from the loss of their breadwinners. While children have been orphaned, education has been impacted, families are struggling to meet their basic needs.</p>
-                                <p className='p-t-15'>As part of its ongoing social initiatives, PayMe India, a Noida-based fintech company, is launching project ‘Saarthi’ to support such families, who have lost their earning members due to the ongoing pandemic. Shri. Mahesh Shukla, founder of PayMe India, strongly believes that project ‘Saarthi’ will have a significant impact in helping families in distress.</p>
-                                <p className='p-t-15'>While PayMe India appreciates the relief measures extended by state and central governments, which has indeed provided immediate support to the families, however a long term support infrastructure has to be built through creation of new employment. This is where project ‘Saarthi’ will play a critical role, by empowering people with necessary skill set to increase their employability and then assisting them in securing an employment.</p>
-                                <p className='p-t-15'>Through project ‘Saarthi’, PayMe India will provide free of cost skill development training to one member from each beleaguered family and further assist them in securing work opportunities. PayMe India have themselves pledged to directly employ people from this program.</p>
-                                <p className='p-t-15 p-b-30'>Driven by strong social values and a pledge to give back to the society, PayMe India is determined be a forerunner in supporting the nation through these testing times. Project ‘Saarthi’ will surely bring hope and relief to numerous families!</p>
+                            <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12  initiative"
+                                 style={{textAlign: "justify"}}>
+                                <h4>PayMe India launches project ‘Saarthi’ to support families impacted by Coivd-19,
+                                    through skill building and creation of employment opportunities.</h4>
+                                <p className='p-t-15'>We are facing a global health crisis unlike any we have seen in
+                                    the last many decades. One that is killing people, spreading human suffering, and
+                                    upending people’s lives. But much more than a health crisis, it is a human, economic
+                                    and social crisis which is attacking societies at their core.</p>
+                                <p className='p-t-15'>With casualties in India touching as high as 4000 per day,
+                                    numerous families have been impacted severely and more so from the loss of their
+                                    breadwinners. While children have been orphaned, education has been impacted,
+                                    families are struggling to meet their basic needs.</p>
+                                <p className='p-t-15'>As part of its ongoing social initiatives, PayMe India, a
+                                    Noida-based fintech company, is launching project ‘Saarthi’ to support such
+                                    families, who have lost their earning members due to the ongoing pandemic. Shri.
+                                    Mahesh Shukla, founder of PayMe India, strongly believes that project ‘Saarthi’ will
+                                    have a significant impact in helping families in distress.</p>
+                                <p className='p-t-15'>While PayMe India appreciates the relief measures extended by
+                                    state and central governments, which has indeed provided immediate support to the
+                                    families, however a long term support infrastructure has to be built through
+                                    creation of new employment. This is where project ‘Saarthi’ will play a critical
+                                    role, by empowering people with necessary skill set to increase their employability
+                                    and then assisting them in securing an employment.</p>
+                                <p className='p-t-15'>Through project ‘Saarthi’, PayMe India will provide free of cost
+                                    skill development training to one member from each beleaguered family and further
+                                    assist them in securing work opportunities. PayMe India have themselves pledged to
+                                    directly employ people from this program.</p>
+                                <p className='p-t-15 p-b-30'>Driven by strong social values and a pledge to give back to
+                                    the society, PayMe India is determined be a forerunner in supporting the nation
+                                    through these testing times. Project ‘Saarthi’ will surely bring hope and relief to
+                                    numerous families!</p>
                             </div>
                             <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12  initiative"
                                  style={{textAlign: "justify"}}>
-                                <form>
+                                <form id='form' name='form'>
                                     <div className="Home-contact-form mt-4">
                                         <h4 className="form-heading text-center">Volunteer and Trainee Registration</h4>
 
@@ -97,7 +157,8 @@ const SocialInitiative = (props) => {
                                                        }}
                                                        value="Volunteer"/>
                                                 <label htmlFor="volunteer">Volunteer</label>
-                                                <input type="radio" id="register" name="registration" defaultChecked='true'
+                                                <input type="radio" id="register" name="registration"
+                                                       defaultChecked='true'
                                                        onChange={(e) => {
                                                            setToggle('false');
                                                        }}
@@ -114,14 +175,15 @@ const SocialInitiative = (props) => {
                                                     onChange={(e) => {
                                                         setName(e.target.value);
                                                     }}
-                                                />
+                                                    required=""/>
 
                                             </div>
                                             <div className="form-group ms-input-group">
                                                 <label className="form-label pb-2">Email</label>
                                                 <input
                                                     name='email'
-                                                    type="text"
+                                                    type="email"
+                                                    pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
                                                     className="form-control ms-form-input"
                                                     placeholder="Enter Email"
                                                     onChange={(e) => {
@@ -133,12 +195,16 @@ const SocialInitiative = (props) => {
                                                 <label className="form-label pb-2">Phone</label>
                                                 <input
                                                     name='phone'
-                                                    type="text"
+                                                    type="tel"
+                                                    maxLength='10'
+                                                    pattern="[0-9]+"
+                                                    onChange={(e) => {
+                                                        e.target.value =
+                                                            e.target.value.replace(/[^0-9.]{10}/g, '').replace(/(\..*)\./g, '$1')
+                                                        setPhone(e.target.value)
+                                                    }}
                                                     className="form-control ms-form-input"
                                                     placeholder="Enter Phone"
-                                                    onChange={(e) => {
-                                                        setPhone(e.target.value);
-                                                    }}
                                                 />
                                             </div>
                                             <div className="form-group ms-input-group">
@@ -151,10 +217,10 @@ const SocialInitiative = (props) => {
                                                     onChange={(e) => {
                                                         setTopic(e.target.value);
                                                     }}
-                                                />
+                                                    required=""/>
                                             </div>
                                         </div>
-                                            <input
+                                        <input
                                             name='topic'
                                             type="button"
                                             className="submitBtn text-center"
