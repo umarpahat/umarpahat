e24be1571116a997c9b4ba0a8792122bb5196acd
Pramod Singh <pramod.singh@paymeindia.in>
2021-09-23 21:01:12 +0530

    end pint changed

--- src/constant.js
+++ src/constant.js
@@ -1,11 +1,11 @@
 // export const API_ENDPOINT = 'http://localhost:8000';  
- //export const API_ENDPOINT = 'https://api.paymeindia.in'
+ export const API_ENDPOINT = 'https://api.paymeindia.in'
 // export const API_ENDPOINT = 'https:///apiv2.paymeindia.in';
 // export const API_ENDPOINT = 'https://apit.paymeindia.in';
 // export const API_ENDPOINT = 'https://api.testing.paymeindia.in';
-export const API_ENDPOINT = 'https://api.staging.paymeindia.in';
+//export const API_ENDPOINT = 'https://api.staging.paymeindia.in';
 
 // export const API_ENDPOINT_STAGING = "https://apiv2.paymeindia.in"
-export const API_ENDPOINT_STAGING = "https:/staging.paymeindia.in";
-// export const API_ENDPOINT_STAGING = "https://apiv2.paymeindia.in";
+//export const API_ENDPOINT_STAGING = "https:/staging.paymeindia.in";
+ export const API_ENDPOINT_STAGING = "https://apiv2.paymeindia.in";
 export const API_ENDPOINT_SAARTHI = 'https://api-csr.paymeindia.in'
