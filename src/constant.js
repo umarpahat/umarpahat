6edcbcff44ce170767be79884518cd4fa9422b9b
Pramod Singh <pramod.singh@paymeindia.in>
2021-12-05 12:21:15 +0530

    testing

--- src/constant.js
+++ src/constant.js
@@ -2,16 +2,20 @@ export let API_ENDPOINT;
 export let API_ENDPOINT_SAARTHI;
 export let API_ENDPOINT_STAGING;
 export let API_ENDPOINT_CITY;
-if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-    console.log("dev code")
-    API_ENDPOINT = 'https://api.staging.paymeindia.in';
-    API_ENDPOINT_STAGING = "https:/staging.paymeindia.in";
-    API_ENDPOINT_SAARTHI = 'https://api-csr.paymeindia.in';
-    API_ENDPOINT_CITY = 'https:staging.paymeindia.in';
-} else {
-    console.log("dev Prod")
-    API_ENDPOINT = "https://api.paymeindia.in"
-    API_ENDPOINT_SAARTHI = 'https://api-csr.paymeindia.in';
-    API_ENDPOINT_STAGING = "https://apiv2.paymeindia.in";
-    API_ENDPOINT_CITY = "https://apiv2.paymeindia.in";
-}
+// if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+//     console.log("dev code")
+//     API_ENDPOINT = 'https://api.staging.paymeindia.in';
+//     API_ENDPOINT_STAGING = "https://staging.paymeindia.in";
+//     API_ENDPOINT_SAARTHI = 'https://api-csr.paymeindia.in';
+//     API_ENDPOINT_CITY =' https://staging.paymeindia.in/api/customer-lead/customer-query/';
+// } else {
+//     console.log("dev Prod")
+//     API_ENDPOINT = "https://api.paymeindia.in"
+//     API_ENDPOINT_SAARTHI = 'https://api-csr.paymeindia.in';
+//     API_ENDPOINT_STAGING = "https://apiv2.paymeindia.in";
+//     API_ENDPOINT_CITY = "https://apiv2.paymeindia.in";
+// }
+API_ENDPOINT = 'https://api.staging.paymeindia.in';
+API_ENDPOINT_STAGING = "https://staging.paymeindia.in";
+API_ENDPOINT_SAARTHI = 'https://api-csr.paymeindia.in';
+API_ENDPOINT_CITY =' https://staging.paymeindia.in/api/customer-lead/customer-query/';
