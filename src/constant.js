6fea1138572ac8b3b8cd9a1a49d3536c0cd54538
Pramod Singh <pramod.singh@paymeindia.in>
2022-01-03 18:09:19 +0530

    facebook pixel code

--- src/constant.js
+++ src/constant.js
@@ -3,18 +3,20 @@ export let API_ENDPOINT_SAARTHI;
 export let API_ENDPOINT_STAGING;
 
 
+API_ENDPOINT = "https://api.staging.paymeindia.in";
+API_ENDPOINT_STAGING = "https://staging.paymeindia.in";
+API_ENDPOINT_SAARTHI = "https://api-csr.paymeindia.in";
 
 
-
-if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
-  console.log("dev code");
-  API_ENDPOINT = "https://api.staging.paymeindia.in";
-  API_ENDPOINT_STAGING = "https://staging.paymeindia.in";
-  API_ENDPOINT_SAARTHI = "https://api-csr.paymeindia.in";
-}
- else {
-  console.log("dev Prod");
-  API_ENDPOINT = "https://api.paymeindia.in";
-  API_ENDPOINT_SAARTHI = "https://api-csr.paymeindia.in";
-  API_ENDPOINT_STAGING = "https://apiv2.paymeindia.in";
-}
+// if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+//   console.log("dev code");
+//   API_ENDPOINT = "https://api.staging.paymeindia.in";
+//   API_ENDPOINT_STAGING = "https://staging.paymeindia.in";
+//   API_ENDPOINT_SAARTHI = "https://api-csr.paymeindia.in";
+// }
+//  else {
+//   console.log("dev Prod");
+//   API_ENDPOINT = "https://api.paymeindia.in";
+//   API_ENDPOINT_SAARTHI = "https://api-csr.paymeindia.in";
+//   API_ENDPOINT_STAGING = "https://apiv2.paymeindia.in";
+// }
